b07796c9a683b427d9878d342c11c5262c04028b
y-miyakaw <y-miyakawa@tribeat.com>
2024-04-29 14:34:26 +0900

    two sum go

--- go/two_sum/two_sum.go
+++ go/two_sum/two_sum.go
@@ -1,5 +1,6 @@
 package twosum
 
+// 時間計算量O(n^2), 空間計算量O(1)
 func twoSum(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
@@ -9,4 +10,18 @@ func twoSum(nums []int, target int) []int {
 		}
 	}
 	return []int{}
+}
+
+// 時間計算量O(n), 空間計算量O(n)
+func twoSum2(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		diff := target - nums[i]
+		if value, exists := m[diff]; exists {
+			return []int{value, i}
+		} else {
+			m[nums[i]] = i
+		}
+	}
+	return nil
 }
--- go/two_sum/two_sum_test.go
+++ go/two_sum/two_sum_test.go
@@ -49,3 +49,47 @@ func Test_twoSum(t *testing.T) {
 		})
 	}
 }
+func Test_twoSum2(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				nums:   []int{2, 7, 11, 15},
+				target: 9,
+			},
+			want: []int{0, 1},
+		},
+		{
+			name: "test2",
+			args: args{
+				nums:   []int{3, 2, 4},
+				target: 6,
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "test3",
+			args: args{
+				nums:   []int{3, 3},
+				target: 6,
+			},
+			want: []int{0, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum2(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

a8aa0138e791efa6d999fdf5d9b4b6cd9ff8c5aa
yuki-miyakawa <127266099+yuki-miyakawa@users.noreply.github.com>
2025-12-29 01:16:10 +0000

    string, array of ts

--- typescript/tutorials/06_string.ts
+++ typescript/tutorials/06_string.ts
@@ -1,2 +1,31 @@
-const str1: string = "helle";
-console.log(len(str1));
+const str1: string = "hello";
+console.log(str1.length);
+
+const str2: string = "こんにちは";
+console.log(str2.length);
+
+const str3: string = "satoh";
+console.log(`ようこそ${str3}さま`);
+console.log("ようこそ%sさま", str3);
+
+const str4: string = "hello world";
+console.log(str4.slice(2));
+console.log(str4.slice(2, 5));
+console.log(str4.slice(-5));
+
+// strconv
+const str5: string = "1234";
+const strnum: number = Number(str5);
+console.log(strnum);
+const str6: string = "1234a";
+const strnum2: number = Number(str6);
+console.log(strnum2);
+
+// join
+const strArr: Array<string> = ["aaa", "bbb", "ccc"];
+console.log(strArr.join(","));
+
+// split
+const str7: string = "hello, morning, evening";
+const strArr2: string[] = str7.split(",");
+console.log(strArr2);
---
+++ typescript/tutorials/07_2d_array.ts
@@ -0,0 +1,9 @@
+const arr2d1: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(arr2d1);
+
+arr2d1[1].push(10);
+console.log(arr2d1);
